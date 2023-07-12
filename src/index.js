e2caf01839f6d1016561ec59fba02d67df4f9ad4
SnakyMz <Soban.syeed@outlook.com>
2023-07-13 01:23:03 +0530

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 import './style.css';
-import updateRotation from './assets/updateRotation.png'
-import downLeft from './assets/downLeft.png'
-import threeDots from './assets/threeDots.png'
+import './assets/updateRotation.png';
+import './assets/downLeft.png';
+import './assets/threeDots.png';
 
 const listArray = [
-    {
-        description: "Morning Meeting",
-        completed: true,
-        index: 0
-    },
-    {
-        description: "List project",
-        completed: false,
-        index: 1
-    },
+  {
+    description: 'Morning Meeting',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'List project',
+    completed: false,
+    index: 1,
+  },
 ];
 
 const list = document.querySelector('#list');
 
 function displayList() {
-    listArray.forEach((object) => {
-        const activity = document.createElement('li');
-        activity.className = 'activity';
-        activity.innerHTML = `
+  listArray.forEach((object) => {
+    const activity = document.createElement('li');
+    activity.className = 'activity';
+    activity.innerHTML = `
         <div class="actDetail">
                     <input type="checkbox" id="${object.index}" class="checkings">
                     <label for="${object.index}">${object.description}</label>
                 </div>
                 <img src="./images/threeDots.png" alt="Option" class="icons optionBtn">
         `;
-        list.appendChild(activity);
-    });
-};
+    list.appendChild(activity);
+  });
+}
 
 window.onload = () => {
-    displayList();
+  displayList();
 };
