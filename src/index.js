dd3930893ab10a219947b9084f8696234c677feb
SnakyMz <Soban.syeed@outlook.com>
2023-07-13 22:47:54 +0530

    Add removing and adding functions

--- src/index.js
+++ src/index.js
@@ -1,38 +1,26 @@
 import './style.css';
 import './assets/updateRotation.png';
 import './assets/downLeft.png';
-import './assets/threeDots.png';
+import './assets/delete.png';
+import ToDos from './modules/fuctionality.js';
 
-const listArray = [
-  {
-    description: 'Morning Meeting',
-    completed: true,
-    index: 0,
-  },
-  {
-    description: 'List project',
-    completed: false,
-    index: 1,
-  },
-];
+const actList = new ToDos();
 
-const list = document.querySelector('#list');
+const addBtn = document.querySelector('.addBtn');
+const addText = document.querySelector('.addText');
 
-function displayList() {
-  listArray.forEach((object) => {
-    const activity = document.createElement('li');
-    activity.className = 'activity';
-    activity.innerHTML = `
-        <div class="actDetail">
-                    <input type="checkbox" id="${object.index}" class="checkings">
-                    <label for="${object.index}">${object.description}</label>
-                </div>
-                <img src="./images/threeDots.png" alt="Option" class="icons optionBtn">
-        `;
-    list.appendChild(activity);
-  });
-}
+addBtn.onclick = () => {
+  actList.addAct(addText.value);
+  addText.value = '';
+};
+
+addText.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter' && addText.value) {
+    actList.addAct(addText.value);
+    addText.value = '';
+  }
+})
 
 window.onload = () => {
-  displayList();
+  actList.displayList();
 };
