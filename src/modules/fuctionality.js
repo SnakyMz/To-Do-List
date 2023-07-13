ee249f39ba9140b2cd3950daeea27fc986d7ee0b
SnakyMz <Soban.syeed@outlook.com>
2023-07-14 00:05:27 +0530

    Fix Linters

--- src/modules/fuctionality.js
+++ src/modules/fuctionality.js
@@ -1,65 +1,65 @@
 export default class ToDos {
-    constructor() {
-        this.listArray = JSON.parse(localStorage.getItem('activities')) || [];
-    }
+  constructor() {
+    this.listArray = JSON.parse(localStorage.getItem('activities')) || [];
+  }
 
     displayList = () => {
-        const list = document.querySelector('#list');
-        list.innerHTML = '';
-        this.listArray.forEach((object, index) => {
-            const activity = document.createElement('li');
-            activity.className = 'activity';
-            const actDetail = document.createElement('div');
-            actDetail.className = 'actDetail';
-            const checkings = document.createElement('input');
-            checkings.type = 'checkbox';
-            checkings.className = 'checkings';
-            actDetail.appendChild(checkings);
-            const desc = document.createElement('input');
-            desc.type = 'text';
-            desc.className = 'desc';
-            desc.value = `${object.description}`;
-            desc.addEventListener('keypress', (e) => {
-                if (e.key === 'Enter' && desc.value) {
-                    this.updateAct(desc.value, (index));
-                }
-            });
-            actDetail.appendChild(desc);
-            activity.appendChild(actDetail);
-            const removeBtn = document.createElement('img');
-            removeBtn.src = '/images/delete.png';
-            removeBtn.alt = 'Delete';
-            removeBtn.className = 'icons removeBtn';
-            removeBtn.addEventListener('click', () => {
-                this.removeAct(index + 1);
-            });
-            activity.appendChild(removeBtn);
-            list.appendChild(activity);
+      const list = document.querySelector('#list');
+      list.innerHTML = '';
+      this.listArray.forEach((object, index) => {
+        const activity = document.createElement('li');
+        activity.className = 'activity';
+        const actDetail = document.createElement('div');
+        actDetail.className = 'actDetail';
+        const checkings = document.createElement('input');
+        checkings.type = 'checkbox';
+        checkings.className = 'checkings';
+        actDetail.appendChild(checkings);
+        const desc = document.createElement('input');
+        desc.type = 'text';
+        desc.className = 'desc';
+        desc.value = `${object.description}`;
+        desc.addEventListener('keypress', (e) => {
+          if (e.key === 'Enter' && desc.value) {
+            this.updateAct(desc.value, (index));
+          }
         });
+        actDetail.appendChild(desc);
+        activity.appendChild(actDetail);
+        const removeBtn = document.createElement('img');
+        removeBtn.src = '/images/delete.png';
+        removeBtn.alt = 'Delete';
+        removeBtn.className = 'icons removeBtn';
+        removeBtn.addEventListener('click', () => {
+          this.removeAct(index + 1);
+        });
+        activity.appendChild(removeBtn);
+        list.appendChild(activity);
+      });
     }
 
     addAct = (value) => {
-        const arrayObject = {};
-        arrayObject.description = value;
-        arrayObject.completed = false;
-        arrayObject.index = (this.listArray.length + 1);
-        this.listArray.push(arrayObject);
-        localStorage.setItem('activities', JSON.stringify(this.listArray));
-        this.displayList();
+      const arrayObject = {};
+      arrayObject.description = value;
+      arrayObject.completed = false;
+      arrayObject.index = (this.listArray.length + 1);
+      this.listArray.push(arrayObject);
+      localStorage.setItem('activities', JSON.stringify(this.listArray));
+      this.displayList();
     }
 
     updateAct = (value, index) => {
-        this.listArray[index].description = value;
-        localStorage.setItem('activities', JSON.stringify(this.listArray));
-        this.displayList();
+      this.listArray[index].description = value;
+      localStorage.setItem('activities', JSON.stringify(this.listArray));
+      this.displayList();
     }
 
     removeAct = (value) => {
-        this.listArray = this.listArray.filter((object) => object.index !== value);
-        this.listArray.forEach((object, index) => {
-            object.index = (index + 1);
-        });
-        localStorage.setItem('activities', JSON.stringify(this.listArray));
-        this.displayList();
+      this.listArray = this.listArray.filter((object) => object.index !== value);
+      this.listArray.forEach((object, index) => {
+        object.index = (index + 1);
+      });
+      localStorage.setItem('activities', JSON.stringify(this.listArray));
+      this.displayList();
     }
 }
