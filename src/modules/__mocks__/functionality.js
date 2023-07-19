f68135a251f38c12b818ab0f62850333a45917f2
SnakyMz <Soban.syeed@outlook.com>
2023-07-20 00:32:41 +0530

    Fix linters

--- src/modules/__mocks__/functionality.js
+++ src/modules/__mocks__/functionality.js
@@ -1,34 +1,34 @@
 export default class ToDos {
-    constructor() {
-        this.listArray = [];
-    }
+  constructor() {
+    this.listArray = [];
+  }
 
     displayList = () => {
-        const testList = document.querySelector('#testList');
-        testList.innerHTML = '';
-        this.listArray.forEach((object) => {
-            const activity = document.createElement('li');
-            activity.innerHTML = `${object.description}`;
-            testList.appendChild(activity);
-        });
+      const testList = document.querySelector('#testList');
+      testList.innerHTML = '';
+      this.listArray.forEach((object) => {
+        const activity = document.createElement('li');
+        activity.innerHTML = `${object.description}`;
+        testList.appendChild(activity);
+      });
     }
 
     addAct = (value) => {
-        const arrayObject = {};
-        arrayObject.description = value;
-        arrayObject.completed = false;
-        arrayObject.index = (this.listArray.length + 1);
-        this.listArray.push(arrayObject);
-        localStorage.setItem('activities', JSON.stringify(this.listArray));
-        this.displayList();
+      const arrayObject = {};
+      arrayObject.description = value;
+      arrayObject.completed = false;
+      arrayObject.index = (this.listArray.length + 1);
+      this.listArray.push(arrayObject);
+      localStorage.setItem('activities', JSON.stringify(this.listArray));
+      this.displayList();
     }
-    
+
     removeAct = (value) => {
-        this.listArray = this.listArray.filter((object) => object.index !== value);
-        this.listArray.forEach((object, index) => {
-            object.index = (index + 1);
-        });
-        localStorage.setItem('activities', JSON.stringify(this.listArray));
-        this.displayList();
+      this.listArray = this.listArray.filter((object) => object.index !== value);
+      this.listArray.forEach((object, index) => {
+        object.index = (index + 1);
+      });
+      localStorage.setItem('activities', JSON.stringify(this.listArray));
+      this.displayList();
     }
 }
--- src/modules/functionality.test.js
+++ src/modules/functionality.test.js
@@ -1,30 +1,26 @@
-jest.mock('./functionality.js');
+import ToDos from './functionality.js';
 
-import ToDos from "./functionality.js";
+jest.mock('./functionality.js');
 const act = new ToDos();
 
 // Test addTask function
 describe('addTask', () => {
-    test('should add a task to the todoList array', () => {
-        document.body.innerHTML = `
+  test('should add a task to the todoList array', () => {
+    document.body.innerHTML = `
     <ul id = "testList">
     </ul>
     `;
-        const ul = document.querySelector('#testList');
-        act.addAct('Morning');
-        expect(ul.childNodes).toHaveLength(1);
-    });
+    const ul = document.querySelector('#testList');
+    act.addAct('Morning');
+    expect(ul.childNodes).toHaveLength(1);
+  });
 });
 
+// Test removeTask function
 describe('removeTask', () => {
-    test('should remove a task from the todoList array', () => {
-        const ul = document.querySelector('#testList');
-        act.removeAct(1);
-        expect(ul.childNodes).toHaveLength(0);
-    });
+  test('should remove a task from the todoList array', () => {
+    const ul = document.querySelector('#testList');
+    act.removeAct(1);
+    expect(ul.childNodes).toHaveLength(0);
+  });
 });
-
-
-
-
-
